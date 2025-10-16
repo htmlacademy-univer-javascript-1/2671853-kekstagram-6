8ccbdbc7babc02ef5ec3f6e62a2efd759437d207
Алёна <alenka160606@gmail.com>
2025-10-16 18:01:15 +0500

    написала функцию для определения возможности встречи в рабочий день

--- js/functions.js
+++ js/functions.js
@@ -13,3 +13,9 @@ function meetingFitsInSchedule(workStart, workEnd, meetingStart, meetingDuration
   // Проверяем, что встреча начинается и заканчивается в пределах рабочего дня
   return meetingStartMinutes >= workStartMinutes && meetingEndMinutes <= workEndMinutes;
 }
+
+console.log(meetingFitsInSchedule('08:00', '17:30', '14:00', 90)); // true
+console.log(meetingFitsInSchedule('8:0', '10:0', '8:0', 120));     // true
+console.log(meetingFitsInSchedule('08:00', '14:30', '14:00', 90)); // false
+console.log(meetingFitsInSchedule('14:00', '17:30', '08:0', 90));  // false
+console.log(meetingFitsInSchedule('8:00', '17:30', '08:00', 900)); // false
